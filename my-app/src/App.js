3eeaac505f3ab616a990a19dd36520457185ecc3
Letania Ferreira <letaniaferreira@gmail.com>
2018-08-13 23:46:18 -0500

    added components folder and header.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+
+//components
+import Header from './components/headerComponent/header';
+
+// includes
 import './Assets/css/default.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+
+        <header />
+
+
       </div>
     );
   }
