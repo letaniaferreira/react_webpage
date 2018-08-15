c72ba7ddb4b973ed0756a7971a1117a5027df345
Letania Ferreira <letaniaferreira@gmail.com>
2018-08-14 22:29:06 -0500

    fixed header bug on Apps.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-//components
 import Header from './components/headerComponent/header';
 
 // includes
@@ -11,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
 
-        <header />
+        <Header />
 
 
       </div>
