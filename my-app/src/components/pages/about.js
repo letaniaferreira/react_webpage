2a38eb4ee8bd1bcb3ca597f12dad63d35b01aff4
Letania Ferreira <letaniaferreira@gmail.com>
2018-09-10 15:37:28 -0500

    added router

---
+++ my-app/src/components/pages/about.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+        <h1>
+          About Me
+        </h1>
+
+        <p>
+          My name is Letania Ferreira and I am based in San Francisco.
+
+          I love puzzles, languages, traveling, eating and cooking.
+
+          In November 2017 I graduated from a women’s software engineering school, where I built Dining Hero and Language Learning.
+
+          I am currently doing an internship at the Texas Advanced Computing Center and I am looking for cool oportunities in fullstack and backend development, specially if related to communication.
+
+          Let's talk and thank you for visiting!
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ my-app/src/components/pages/professional.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Professional extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+        <h1>
+          Professional
+        </h1>
+
+        <p>
+            Skills
+      
+            Languages | Python, JavaScript, HTML, CSS, SQL
+            Frameworks & Libraries | Flask, SQLAlchemy, Django, AngularJS, Bootstrap, Jinja, NLTK, Spacy
+            Database & Industry Tools | PostgreSQL, Git, GitHub, Command Line, Docker
+            Tools/Software | Praat, SPSS
+            Natural Languages | Portuguese: Native, English: Near native, Spanish: Near native, French: Fluent, Italian: Conversational
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default Professional;
