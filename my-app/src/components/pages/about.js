f205a0a12938de67dbadb47272273e188c685298
Letania Ferreira <letaniaferreira@gmail.com>
2018-11-02 18:32:11 -0700

    Update information about internship

--- my-app/src/components/pages/about.js
+++ my-app/src/components/pages/about.js
@@ -17,7 +17,7 @@ class About extends Component {
 
           In November 2017 I graduated from a women’s software engineering school, where I built Dining Hero and Language Learning.
 
-          I am currently doing an internship at the Texas Advanced Computing Center and I am looking for cool oportunities in fullstack and backend development, specially if related to communication.
+          In September 2018 I wrapped up an internship at the Texas Advanced Computing Center, where I worked on DesignSafe, a natural hazard portal, which allows researchers to submit and check the results of jobs to supercomputers at the center. In my free time, I have been building websites, trying to learn new technologies and having some fun trying to solve code challenges. Please check my Github to see some of my projects. I am looking for cool oportunities in fullstack and backend development, specially if related to communication.
 
           Let's talk and thank you for visiting!
         </p>
