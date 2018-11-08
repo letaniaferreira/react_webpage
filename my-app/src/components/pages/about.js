3787f7da81a7db6a3ad642ef1a4ee9ff8616a477
Letania Ferreira <letaniaferreira@gmail.com>
2018-11-08 15:54:25 -0800

    Add formating to about

--- my-app/src/components/pages/about.js
+++ my-app/src/components/pages/about.js
@@ -10,18 +10,18 @@ class About extends Component {
           About Me
         </h1>
 
-        <p>
-          My name is Letania Ferreira and I am based in San Francisco.
-
-          I love puzzles, languages, traveling, eating and cooking.
-
-          In November 2017 I graduated from a women’s software engineering school, where I built Dining Hero and Language Learning.
-
-          In September 2018 I wrapped up an internship at the Texas Advanced Computing Center, where I worked on DesignSafe, a natural hazard portal, which allows researchers to submit and check the results of jobs to supercomputers at the center. In my free time, I have been building websites, trying to learn new technologies and having some fun trying to solve code challenges. Please check my Github to see some of my projects. I am looking for cool oportunities in fullstack and backend development, specially if related to communication.
-
-          Let's talk and thank you for visiting!
-        </p>
-
+        <div>My name is Letania Ferreira and I am based in San Francisco.</div>
+          <br/>
+        <div>I love puzzles, languages, traveling, eating and cooking.</div>
+        <br/>
+        <div>In November 2017 I graduated from a women’s software engineering school, where I built Dining Hero and Language Learning.<br/>
+        <br/>
+        <div>In September 2018 I wrapped up an internship at the Texas Advanced Computing Center, where I worked on DesignSafe, a natural hazard portal, which allows researchers to submit and check the results of jobs to supercomputers at the center.</div>
+        <br/>
+        <div> In my free time, I have been building websites, trying to learn new technologies and having some fun trying to solve code challenges. Please check my Github to see some of my projects. I am looking for cool oportunities in fullstack and backend development, specially if related to communication.</div>
+        <br/>
+        <div>Let's talk and thank you for visiting!</div>
+        </div>
       </div>
     );
   }
