3cb59f5ea4921727c9d693ba841081a5a7909ae7
Letania Ferreira <letaniaferreira@gmail.com>
2018-11-10 23:54:38 -0800

    Add professional info

--- my-app/src/components/pages/professional.js
+++ my-app/src/components/pages/professional.js
@@ -40,6 +40,96 @@ class Professional extends Component {
             - Created asynchronous Celery tasks for long-running processes<br/>
             - Refactored codebase and increased test coverage<br/>
             <br/>
+
+            <br/>
+            <br/>
+            <b>Assistant professor</b>, <a href="http://www.ufrpe.br/">Universidade Federal Rural de Pernambuco</a> (Cabo, Brazil)     <br/> 2015 - 2017 <br/>
+            - Published peer reviewed articles, led research projects and collaborated with other scholars and advisees <br/>
+            - Organized <a href="http://semanadasengenharias.com/2016/">Segunda Semana das Engenharias</a> and participated in many committees
+            <br/>
+            <br/>
+            <b>Postdoctoral Fellow</b>, <a href="https://www.ufpe.br/">Universidade Federal de Pernambuco</a> (Recife, Brazil)                <br/>2013 - 2015<br/>
+            - Published peer reviewed papers by developing acoustic and statistic voice analysis<br/>
+            - Built large variational research database and organized IV SIS VOGAIS<br/>
+            <br/>
+            <b>Senior Linguist</b>, <a href="http://www.cesar.org.br/">C.E.S.A.R.</a> (Recife, Brazil)                   <br/>Spring 2013<br/>
+            - Managed group of language specialists developing database and semantic classification<br/>
+            - Coordinated with client and colaborated with testers and software engineers
+            <br/>
+            <br/>
+            <b>Assistant professor</b>, <a href="https://www.augustana.edu/">Augustana College</a> (Rock Island, IL)                        <br/>2008 - 2013<br/>
+            - Developed collaborative, multidisciplinary research resulting in publications, conference presentations and well-received funding applications<br/>
+            - Directed <a href="https://www.augustana.edu/academics/core/study-abroad/summer-programs/spanish-in-the-andes">Summer in the Andes study abroad program</a> 
+            <br/>
+            <br/>
+            <b>Visiting instructor</b>, <a href="https://www.iwu.edu/">Illinois Wesleyan University</a> (Bloomington, IL)                 <br/>2007 - 2008<br/>
+             - Led group work, advised students, delivered talks and lectured classes
+            <br/>
+            <br/>
+            <b>Lectrice d’anglais</b>, <a href="https://www.uliege.be/cms/c_8699436/en/portail-uliege">Université de Liège</a> (Liège, Belgium)                        <br/>2006 - 2007<br/>
+            - Led group activities, evaluated students speech skills and recommended enhancement exercises
+            <br/>
+            <br/>
+            <b>Ph.D Candidate and Teaching Assistant</b>, <a href="http://illinois.edu/">University of Illinois at Urbana-Champaign</a>             <br/>2000 - 2006<br/>
+            -Completed a dissertation project on the acoustic correlates of stress in Spanish and Brazilian Portuguese<br/>
+            -Taught Spanish and Portuguese courses resulting in inclusion on excellent teacher list
+            <br/>
+            <br/>
+            <b>Master Candidate and Teaching Assistant</b>, <a href="https://www.uga.edu/">University of Georgia at Athens</a>             <br/>1998 - 2000<br/>
+            -Completed a thesis project on Comparative literary pieces<br/>
+            -Taught Portuguese courses and tutored athlete students
+            <br/>
+            <br/>
+           
+            <h3>Education</h3>
+           
+            <h4><b>Software Engineering</b>, Hackbright Academy, San Francisco, CA
+            <br/>
+            <br/>
+            <b>Ph. D. Romance Linguistics</b> (focus Acoustic Phonetics), The University of Illinois at Urbana-Champaign   
+            <br/>
+            <br/>
+            <b>MA in Romance Languages</b> The University of Georgia at Athens
+            <br/>
+            <br/>
+            <b> B. A. in Portuguese and English</b>, Universidade Federal de Pernambuco – Recife, Brazil</h4>
+            <br/>
+           
+
+            <h3>Selected Publications</h3>
+            <h4>
+            Sessarego, S.; Ferreira, L. (2016) <a href="https://benjamins.com/#catalog/books/ihll.8.12ses/details">Spanish and Portuguese Parallels: Impoverished Number Agreement as a Vernacular Feature of Two Rural Dialects. </a>Issues in Hispanic and Lusophone Linguistics, 8, p. 283-304<br/>
+            Ferreira, L. (2015) <a href="http://www.uel.br/revistas/uel/index.php/signum/article/viewFile/21912/17770">They don’t agree, do they? Signs of verbal agreement productivity in Recife. </a> Signum: Estudos da Linguagem, 18 (1), p. 189-212. <br/>
+            Ferreira, L. and Holt, D. E. (2014) <a href="https://www.researchgate.net/publication/305317755_On_the_partially_divergent_phonology_of_Spanish_Portuguese_and_points_in_between"> On the partially divergent phonology of Spanish, Portuguese and points in between. </a> Issues in Hispanic and Lusophone Linguistics, 1, p. 123-150.<br/>
+            Ferreira, L. (2011) <a href="http://www.letras.ufrj.br/poslinguistica/revistalinguistica/wp-content/uploads/2011/11/peaks-and-plateaux3.pdf"> Peaks and Plateaux: Different F0 Patterns Representing the Same Category. </a> Revista Linguística, UFRJ, 7 (1), 1-13.
+            </h4>
+            <br/>
+            <h3>Scholarships, Grants and Awards</h3>
+            <h4>
+            Desenvolvimento Científico Regional FACEPE/CNPq, Brazil, jun 2013.<br/>
+            New Faculty Research Grant, Augustana College, Rock Island, Jan 2011.<br/>
+
+            New Faculty Research Grant, Augustana College, Rock Island, Jan 2010.<br/>
+
+            Academic Scholarship, Université de Liège, Belgium, Sept. 2006 - May 2007.<br/>
+
+            Excellence in Teaching Award, University of Illinois at Urbana-Champaign, Spring 2002, Spring 2004, Spring 2005 and Fall 2005.<br/>
+
+            CLACS grant for field research, Center of Latin American and Caribbean Studies, University of Illinois at Urbana-Champaign, Summer 2001.<br/>
+
+            Straight A’s Award, Intensive English Institute, University of Illinois at Urbana-Champaign, 1998.<br/>
+
+            Most-Improved IEI TOEFL score, University of Illinois at Urbana-Champaign, Spring 1998.<br/>
+
+            </h4>
+            <h3>Work Related Activities</h3>
+            <h4>
+            <b>Founding Member</b>, NEI Study Group                                     <br/>
+            <b>Manuscript Reviewer</b>, several publications                            <br/>
+            <b>Organizing Team Member</b>, IV SIS-vogais, II SEENG conferences          <br/>
+            <b>Graduate Student's Representative</b>, University of Georgia             <br/>
+            </h4>
+ 
         
 
       </div>
